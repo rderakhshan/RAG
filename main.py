200e2db0bdf50110c7f2739469fdf105ddaa7854
rderakhshan <riemann.derakhshan@gmail.com>
2025-05-14 17:10:07 +0200

    Modified main.py

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ from src.packages.utils.chroma_utils import vectorstore
 from src.packages.utils.pydantic_models import ModelName
 from src.packages.langchain.langchain_utils import get_rag_chain
 from src.packages.utils.pydantic_models import QueryInput, QueryResponse, DeleteFileRequest
-from src.packages.utils.db_utils import ( create_db, get_db, get_all_files, get_file_by_id, get_file_by_name, delete_file_by_id,)
+from src.packages.utils.db_utils import create_db, get_db, get_all_files, get_file_by_id, get_file_by_name, delete_file_by_id
 
 
 # Loading environment variables from .env file
 load_dotenv()
-
-
-
-
 
 
 # logging.basicConfig(filename='app.log', level=logging.INFO)
